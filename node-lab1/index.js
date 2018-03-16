a36f8450b00f8331269c1cb3dfdc007f8aeee06f
Paul <pauldalydev@gmail.com>
2018-03-16 12:47:27 +0000

    Changed to express app

--- node-lab1/index.js
+++ node-lab1/index.js
@@ -1,27 +1,31 @@
-// Load the http module to create a http server.
-import http from 'http';
 import dotenv from 'dotenv';
-import greeting from './greeting.js';
+import express from 'express';
+import greeting from './greeting';
 
 dotenv.config();
 
+const app = express();
+
 const port = process.env.PORT;
-// Configure our HTTP server to respond with Hello World to all requests.
-const server = http.createServer((req, res)=>{
+
+app.use(express.static('public'));
+
+// add route for /greeting
+app.get('/greeting', (req, res)=>{
   let lang = req.headers['accept-language'];
   const defaultLang='en';
-  if(!greeting[lang]) lang=defaultLang;
+  if (!greeting[lang]) lang=defaultLang;
   const response={
     lang: lang,
     message: greeting[lang],
   };
-  
-  res.writeHead(200, {'Content-Type':'text/plain',
-                        'Content-Language':response.lang});
+
+  res.writeHead(200, {'Content-Type': 'text/plain',
+                      'Content-Language': response.lang});
   res.end(response.message);
 });
 
-server.listen(port);
 
-// Put a friendly message on the terminal
-console.log(`Server running at ${port}`);
+app.listen(port, () => {
+  console.info(`Server running at ${port}`);
+});
