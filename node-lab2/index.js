f9154c290bffc398fbe7c78926c0d5bef9e7880f
Paul <pauldalydev@gmail.com>
2018-03-18 00:20:45 +0000

    added routing for add,update,delete posts

--- node-lab2/index.js
+++ node-lab2/index.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import contactsRouter from './api/contacts';
 import bodyParser from 'body-parser';
-
+import postsRouter from './api/posts';
 dotenv.config();
 
 const app = express();
@@ -13,10 +13,11 @@ app.use(express.static('public'));
 
 app.use('/api/contacts', contactsRouter);
 app.use(express.static('public'));
-
+app.use('/api/posts', postsRouter);
 //configure body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
